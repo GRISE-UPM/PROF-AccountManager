58a9739183d8ab4c13bef1d30356ef075ae128ff
odieste <odieste@fi.upm.es>
2021-11-17 11:25:20 +0100

    Proyecto actualizado a Jupiter

--- src/test/java/es/upm/grise/profundizacion/AccountManager/dataAccess/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion/AccountManager/dataAccess/smokeTest.java
@@ -1,8 +1,8 @@
 package es.upm.grise.profundizacion.AccountManager.dataAccess;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class smokeTest {
 
